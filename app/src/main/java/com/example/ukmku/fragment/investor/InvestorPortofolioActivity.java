57ef6b529371518ff91cec2db6dac617d29843fe
Novans <novannjsg9@gmail.com>
2020-02-28 17:44:23 +0700

    4th Update

---
+++ app/src/main/java/com/example/ukmku/fragment/investor/InvestorPortofolioActivity.java
@@ -0,0 +1,16 @@
+package com.example.ukmku.fragment.investor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ukmku.R;
+
+public class InvestorPortofolioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investor_portofolio);
+    }
+}
